11cecee7ef2dc35e754a944709d8bfafc7b99e74
marlonencarnacion156 <66214009+marlonencarnacion156@users.noreply.github.com>
2020-11-13 22:35:41 -0400

    Two-way Data Binding

--- Clase1/src/app/app.module.ts
+++ Clase1/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PresentacionComponent } from './presentacion/presentacion.component';
     PresentacionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
